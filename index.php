c85045d2bc905cfbd9149124c257d10fc0224cf4
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-03-03 13:51:19 +0100

    establishing database connection

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * File: index.php
+ * User: Dylan Schirino
+ * Date: 3/03/16
+ * Time: 13:44
+ */
+
+$connection = new PDO('mysql:dbname=biblio;host=localhost','root','root'); // Authentification a la base de données dbname biblio
